8952b303aeaa7ccbfed25dce4902143eb7a46b35
bivex <209128140+bivex@users.noreply.github.com>
2025-12-25 13:57:00 +0200

    Update copyright timestamp in useLazyHttp composable

--- nuxt/composables/useLazyHttp.ts
+++ nuxt/composables/useLazyHttp.ts
@@ -7,7 +7,7 @@
  * https://github.com/bivex
  *
  * Created: 2025-12-25T11:55:41
- * Last Updated: 2025-12-25T11:56:28
+ * Last Updated: 2025-12-25T11:56:46
  *
  * Licensed under the MIT License.
  * Commercial licensing available upon request.
