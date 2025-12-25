86c6cd6aee66ebf1fe5e66b3b7e843d06ac952c1
bivex <209128140+bivex@users.noreply.github.com>
2025-12-25 14:06:02 +0200

    fix: update Nuxt configuration settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@
  * https://github.com/bivex
  *
  * Created: 2025-12-25T11:37:28
- * Last Updated: 2025-12-25T12:05:46
+ * Last Updated: 2025-12-25T12:05:51
  *
  * Licensed under the MIT License.
  * Commercial licensing available upon request.
